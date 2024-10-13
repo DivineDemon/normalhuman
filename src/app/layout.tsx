7fb2be51b4d74993a7418196dc0e5ee55fbe8af0
DivineDemon <supame123@gmail.com>
2024-10-13 21:22:23 +0500

    Clerk Setup!

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Suspense } from "react";
 
+import { ClerkProvider } from "@clerk/nextjs";
+
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -15,16 +17,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <head>
-        <link
-          rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📦</text></svg>"
-        />
-      </head>
-      <body className="antialiased">
-        <Suspense>{children}</Suspense>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <head>
+          <link
+            rel="icon"
+            href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📦</text></svg>"
+          />
+        </head>
+        <body className="antialiased">
+          <Suspense>{children}</Suspense>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- src/env/server.ts
+++ src/env/server.ts
@@ -4,6 +4,18 @@ import { ZodError, z } from "zod";
 export const env = createEnv({
   server: {
     NODE_ENV: z.enum(["development", "production"]),
+    CLERK_SECRET_KEY: z.string().regex(/^sk_(test|live)_\w+$/, {
+      message:
+        "Invalid secret key format. It should start with 'sk_' followed by 'test' or 'live', and then a series of characters.",
+    }),
+    CLERK_SIGN_IN_URL: z.string().regex(/^\/[a-zA-Z0-9\-_\/]*$/, {
+      message:
+        "Invalid URL path format. It should start with a forward slash ('/') and contain only alphanumeric characters, dashes, underscores, or additional forward slashes.",
+    }),
+    CLERK_SIGN_UP_URL: z.string().regex(/^\/[a-zA-Z0-9\-_\/]*$/, {
+      message:
+        "Invalid URL path format. It should start with a forward slash ('/') and contain only alphanumeric characters, dashes, underscores, or additional forward slashes.",
+    }),
   },
   onValidationError: (error: ZodError) => {
     console.error(
